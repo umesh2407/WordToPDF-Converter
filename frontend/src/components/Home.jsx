49c44fbcfeb4fd3ce95515933bbb4796736bd585
umesh2407 <choudharyumesh986@gmail.com>
2024-05-30 15:47:01 +0530

    final push

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -20,7 +20,7 @@ function Home() {
     formData.append("file", selectedFile);
     try {
       const response = await axios.post(
-        "http://localhost:3000/convertFile",
+        "https://wordtopdf-converter-backend.onrender.com/convertFile",
         formData,
         { responseType: "blob" }
       );
