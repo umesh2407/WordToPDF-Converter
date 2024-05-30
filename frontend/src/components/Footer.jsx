5a043995e073ec16691962967ecf09fcabc7f171
umesh2407 <choudharyumesh986@gmail.com>
2024-05-30 11:45:29 +0530

    added footer

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Footer() {
+  return (
+   <>
+   <div className='items-center justify-center'>
+    <hr className='border-black'/>
+<h1 className='text-center py-3 text-sm'>Copyrights @2024 Umesh Choudhary</h1>
+   </div>
+   </>
+  )
+}
+
+export default Footer
